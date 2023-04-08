011891149b6ec78f392240b170e9fa7d52591a3e
Vitaliy Roy <vitaliybrandet@gmail.com>
2023-04-08 15:12:01 +0300

    Добавьте файлы проекта.

---
+++ lr20/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace lr20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_start = new System.Windows.Forms.TextBox();
+            this.txt_finish = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.txt_start.Location = new System.Drawing.Point(60, 50);
+            this.txt_start.Name = "textBox1";
+            this.txt_start.Size = new System.Drawing.Size(93, 20);
+            this.txt_start.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.txt_finish.Location = new System.Drawing.Point(60, 99);
+            this.txt_finish.Name = "textBox2";
+            this.txt_finish.Size = new System.Drawing.Size(177, 20);
+            this.txt_finish.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введіть рік";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_finish);
+            this.Controls.Add(this.txt_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_start;
+        private System.Windows.Forms.TextBox txt_finish;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lr20/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Власне виключення для обробки помилок пов'язаних з високосним роком
+        public class LeapYearException : Exception
+        {
+            public LeapYearException(string message)
+                : base(message)
+            {
+            }
+        }
+
+        public class LeapYearChecker
+        {
+            public static bool IsLeapYear(int year)
+            {
+                // Перевірка чи є рік від'ємним, якщо так - виключення
+                if (year < 0)
+                {
+                    throw new LeapYearException("Рік не може бути від'ємний.");
+                }
+
+                if (year % 4 != 0)
+                {
+                    return false;
+                }
+
+                if (year % 100 != 0)
+                {
+                    return true;
+                }
+
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int year = int.Parse(txt_start.Text);
+                // Визначення чи є рік високосним
+                bool isLeapYear = LeapYearChecker.IsLeapYear(year);
+
+                if (isLeapYear == true)
+                {
+                    txt_finish.Text = "Рік високосний.";
+                }
+                else
+                {
+                    txt_finish.Text = "Рік не високосний.";
+                }
+            }
+            catch (FormatException)
+            {
+                // Обробка помилки форматування введеного значення
+                txt_finish.Text = "Неправильне значення.";
+            }
+            catch (OverflowException)
+            {
+                // Обробка помилки переповнення типу int
+                txt_finish.Text = "Введене значення занадто довге.";
+            }
+            catch (LeapYearException ex)
+            {
+                // Обробка помилки з використанням власного виключення
+                txt_finish.Text = ex.Message;
+            }
+            catch (Exception)
+            {
+                // Обробка будь-якого іншого виключення
+                throw;
+            }
+        }
+    }
+}
